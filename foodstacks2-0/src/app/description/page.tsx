004c470e7ed1d2f945a7d74654928341f2607975
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2024-11-17 11:10:30 -0800

    Add initial links in description page

---
+++ foodstacks2-0/src/app/description/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const Description = () => {
+  return (
+    <main>
+      <div>Description</div>
+      <button>
+        <Link href="/">back to main page</Link>
+      </button>
+    </main>
+  );
+};
+
+export default Description;
