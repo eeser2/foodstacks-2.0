55d2aac44ff09106b716b8ce5af787d59ba43cf1
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2024-12-14 12:09:16 -0800

    Test whether the preferences is being passed correctly

--- foodstacks2-0/src/app/get_info/route.ts
+++ foodstacks2-0/src/app/get_info/route.ts
@@ -1,17 +1,34 @@
 import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
+import path from "path";
 
-export async function GET() {
-  const res = await fetch(
+export async function POST(req: Request) {
+  const apiKey = await fs.readFile(
+    path.join(process.cwd(), "config.json"),
+    "utf8"
+  );
+  // Get the payload from the request
+  const { typeOfFood, location, distance } = await req.json();
+
+  // Set query parameter values for TripAdvisor API
+  const category = "restaurants";
+  const address = location;
+  // Create a search query based on the type of food, location, and distance
+  const searchQuery = `${typeOfFood} near ${location} within ${distance}`;
+  const data = await fetch(
     "https://api.content.tripadvisor.com/api/v1/location/search",
     {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "API-Key": process.env.DATA_API_KEY,
+        key: apiKey,
+        searchQuery: searchQuery,
+        category: category,
+        address: address,
       },
     }
   );
-  const data = await res.json();
 
+  console.log(data);
   return Response.json({ data });
 }
