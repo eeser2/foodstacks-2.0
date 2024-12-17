46b0e08587e8de05beeab5f13538487f85bbc3be
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2024-12-16 18:01:25 -0800

    Remove distance from Preferences component

--- foodstacks2-0/src/app/components/Preferences.tsx
+++ foodstacks2-0/src/app/components/Preferences.tsx
@@ -7,12 +7,10 @@ import { useRouter } from "next/navigation";
 const Preferences = () => {
   const [typeOfFood, setTypeOfFood] = useState("");
   const [location, setLocation] = useState("");
-  const [distance, setDistance] = useState(1);
   const [location_name, setLocationName] = useState("");
   const [data, setData] = useState<{
     typeOfFood: string;
     location: string;
-    distance: number;
     location_name: string;
   } | null>(null);
   const [isLoading, setLoading] = useState(true);
@@ -24,7 +22,6 @@ const Preferences = () => {
         setData(data);
         setTypeOfFood(data.typeOfFood || ""); // Set initial state from fetched data
         setLocation(data.location || ""); // Set initial state from fetched data
-        setDistance(data.distance || 1); // Set initial state from fetched data
         setLocationName(data.location_name || "");
         setLoading(false);
       })
@@ -32,14 +29,6 @@ const Preferences = () => {
   }, []);
 
   const router = useRouter();
-
-  const handleSliderChange = (value: number | number[]) => {
-    if (Array.isArray(value)) {
-      setDistance(value[0]);
-    } else {
-      setDistance(value);
-    }
-  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -53,17 +42,13 @@ const Preferences = () => {
         body: JSON.stringify({
           typeOfFood,
           location,
-          distance,
           location_name,
         }),
       });
 
       if (response.ok) {
         const result = await response.json();
-        console.log(result.message); // Success message
-        router.push(
-          `/?typeOfFood=${typeOfFood}&location=${location}&distance=${distance}`
-        );
+        router.push(`/?typeOfFood=${typeOfFood}&location=${location}`);
       } else {
         const error = await response.json();
         console.error("Error:", error.error);
@@ -113,24 +98,6 @@ const Preferences = () => {
             onChange={(e) => setLocation(e.target.value)}
             className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:text-blue-700"
           />
-        </div>
-        <div className="mt-6 space-y-2">
-          <label
-            htmlFor="location-slider"
-            className="block text-gray-700 font-medium"
-          >
-            Distance:
-          </label>
-          <Slider
-            step={1}
-            maxValue={50}
-            minValue={1}
-            value={distance}
-            onChange={handleSliderChange}
-            className="max-w-md"
-            id="location-slider"
-          />
-          <div className="text-gray-700 mt-2">{distance} miles</div>
         </div>
         <button type="submit" className="text-black btn btn-primary">
           Save
